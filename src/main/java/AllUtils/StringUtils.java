42ff7dcd04df530c58e733d3af4db228fd48b82a
Leong Paeg-Hing <Leong Paeg-Hing@LAPTOP-8PMDHJLO>
2021-01-11 22:23:57 +0100

    ajout dans StringUtils

--- src/main/java/AllUtils/StringUtils.java
+++ src/main/java/AllUtils/StringUtils.java
@@ -21,4 +21,37 @@ public class StringUtils {
     public static void afficherTitre(String nom, String prénom, String matricule){
         System.out.println(nom+"-"+prénom+"-"+matricule);
     }
+    
+    /**
+ * 
+ * @param mot entré par l'utilisateur
+ * @param lettre la lettre dont il faut compter le nombre d'occurrences
+ * @return retourne le nombre d'occurrences pour la lettre donnée, dans le mot en question
+ */
+    static int nombreLettres(String mot, char lettre) {
+        int cmt = 0;
+        for (int i = 0; i < mot.length(); i++) {
+            char c1 = mot.charAt(i);
+            if (c1 == lettre) {
+                cmt++;
+            }
+        }
+        return cmt;
+    }
+    
+/**
+ * 
+ * 
+ * @param mot mot entré par l'utilisateur
+ * @return retourne le nombre d'occurrences de la lettre la plus fréquente
+ */
+    static int occurrencesLettres(String mot) {
+        int max = 0;
+        for (int i = 0; i < mot.length(); i++) {
+            if (nombreLettres(mot, mot.charAt(i)) > max) {
+                max = nombreLettres(mot, mot.charAt(i));
+            }
+        }
+        return max;
+    }
 }
