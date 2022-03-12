a3df571f7a99fd43336e04d35de81bca533db104
Paeg-Hing Leong <56133@etu.he2b.be>
2022-03-12 14:55:35 +0100

    test git

--- ProjetTest/main.cpp
+++ ProjetTest/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+enum TestValue {LOL = 'L'};
 
 int main()
 {
-    cout << "Hello Monde!" << endl;
-    cout << "Adios World!" << endl;
+    std::vector<int> a {};
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    a.push_back(5);
+    for(auto i : a)
+    {
+        cout << i << endl;
+    }
     return 0;
 }
